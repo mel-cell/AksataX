25b801330357061a4cd6ae3ae6080b6e6f93791f
unknown <athoillahvidyfairuz11a@gmail.com>
2026-06-09 18:15:47 +0700

    mel ini arahnya ke mana

---
+++ app/[local]/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import { Navbar } from "@/components/layout/navbar";
+import { Sidebar } from "@/components/layout/sidebar";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1">{children}</main>
+      </div>
+    </div>
+  );
+}
